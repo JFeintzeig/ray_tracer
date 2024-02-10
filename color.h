da49b36ad0995bd4956f794b48d1ad510d2db687
JFeintzeig <JFeintzeig@users.noreply.github.com>
2024-02-10 12:35:58 -0500

    gamma encode

--- color.h
+++ color.h
@@ -5,12 +5,21 @@
 #include "interval.h"
 #include "vec3.h"
 
+double encode_to_gamma(double value) {
+  return sqrt(value);
+}
+
 void write_color(FILE *fd, color_t c) {
   static const interval_t intensity = {.min = 0.0, .max = 0.99999};
+
+  double r = encode_to_gamma(c.e[0]);
+  double g = encode_to_gamma(c.e[1]);
+  double b = encode_to_gamma(c.e[2]);
+
   fprintf(fd, "%d %d %d\n",
-          (int)(clamp(c.e[0], intensity)*256.0),
-          (int)(clamp(c.e[1], intensity)*256.0),
-          (int)(clamp(c.e[2], intensity)*256.0));
+          (int)(clamp(r, intensity)*256.0),
+          (int)(clamp(g, intensity)*256.0),
+          (int)(clamp(b, intensity)*256.0));
 }
 
 #endif // !COLOR_H
